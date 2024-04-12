dfe5e6762c0df1623fedad9746d0f66b6eac7a3a
Richard O'Dwyer <amnesia@Richards-MacBook-Pro.local>
2024-04-12 19:45:24 +0300

    deploy

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/data/helloworld.db,
+        'NAME': '/data/helloworld.db',
     }
 }
 
